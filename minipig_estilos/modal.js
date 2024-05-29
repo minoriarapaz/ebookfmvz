4973871c511056d2cee43adeee548e6ccf18c2e6
minoriarapaz <krlos.ivan@gmail.com>
2024-05-29 14:06:50 -0600

    error persiste
    Correccion de error en responsive

--- minipig_estilos/modal.js
+++ minipig_estilos/modal.js
@@ -7,14 +7,7 @@ console.log("la nueva var Scroll es " + Scroll);
 
 
 /*script que bloquea scroll al principio*/
-/*window.onload = function() {
-	noScroll();
-}*/
 
-function noScroll() {
-	document.body.style.overflow = "hidden";
-	console.log("var Scroll es 1, scroll NO funciona");
-}
 
 function activarScroll() {
 	document.body.style.overflow = "auto";
@@ -59,26 +52,6 @@ function verBiblio() {
 }
 
 
-function cerrarLegal() {
-	document.getElementById("mLegal").style.display=null;
-}
-
-function verLegal() {
-			if (L==1) {
-				document.getElementById("mLegal").style.display="block";
-				document.getElementById("mLegal").style.opacity="1";
-				document.body.style.overflow = "hidden";
-				noScroll();
-				L=2;
-			} else {
-				setTimeout(cerrarBiblio, 400);
-				document.getElementById("mLegal").style.opacity="0";
-				cerrarIndice(); /*Cierra menu desplegable*/
-				document.body.style.overflow = "auto";
-				activarScroll();
-				L=1;
-			}
-}
 
 		
 
